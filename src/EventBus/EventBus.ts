5009ac5e8c206da3e05efd7ca22536494ebc96a3
Daniel Mecke <daniel.mecke@cunningsoft.de>
2022-12-17 14:31:54 +0100

    added event bus

---
+++ src/EventBus/EventBus.ts
@@ -0,0 +1,43 @@
+import Event from './Event';
+import ListenerId from './ListenerId';
+
+export default class EventBus {
+    private static _instance = new EventBus();
+    private subscribers: Map<string, Map<ListenerId, { priority: number, callback: (event: Event) => void }>> = new Map();
+
+    private constructor() {}
+
+    static get instance(): EventBus {
+        return EventBus._instance;
+    }
+
+    subscribe(name: string, callback: (event: Event) => void, priority = 100): ListenerId {
+        if (!this.subscribers.has(name)) {
+            this.subscribers.set(name, new Map());
+        }
+
+        const listenerId = new ListenerId();
+        this.subscribers.get(name)?.set(listenerId, { priority, callback });
+
+        return listenerId;
+    }
+
+    unsubscribe(listenerId: ListenerId): void {
+        this.subscribers.forEach(callback => callback.delete(listenerId));
+    }
+
+    unsubscribeMultiple(listenerIds: ListenerId[]): void {
+        listenerIds.forEach(listenerId => this.unsubscribe(listenerId));
+    }
+
+    dispatch(event: Event): void {
+        if (!this.subscribers.has(event.name)) {
+            return;
+        }
+
+        const entries = this.subscribers.get(event.name)?.entries() ?? [];
+        const subscribers = new Map([...entries].sort());
+
+        subscribers.forEach(subscriber => subscriber.callback(event));
+    }
+}
