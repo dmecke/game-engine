4dcfb4c04f1efca22789cd54a42853f5f88a7914
Daniel Mecke <daniel.mecke@cunningsoft.de>
2022-12-17 14:40:12 +0100

    added asset loaders

---
+++ src/AssetLoader/AudioLoader.ts
@@ -0,0 +1,44 @@
+import {Howl} from 'howler';
+
+export default class AudioLoader {
+    static readonly instance = new AudioLoader();
+    private files: Map<string, Howl> = new Map();
+
+    private constructor() {}
+
+    static loadFiles(files: string[]): Promise<void> {
+        return new Promise(resolve => {
+            let loaded = 0;
+            for (const file of files) {
+                const howl = new Howl({
+                    src: `audio/${file}`,
+                    onload: () => {
+                        loaded++;
+                        if (loaded === files.length) {
+                            resolve();
+                        }
+                    }
+                });
+                AudioLoader.instance.files.set(file, howl);
+            }
+        });
+    }
+
+    getFile(file: string): Howl {
+        const howl = this.files.get(file);
+        if (!howl) {
+            throw new Error(`Could not find audio file with name "${file}".`);
+        }
+
+        return howl;
+    }
+
+    getFileOrNull(file: string): Howl|null {
+        const howl = this.files.get(file);
+        if (!howl) {
+            return null;
+        }
+
+        return howl;
+    }
+}
---
+++ src/AssetLoader/ImageLoader.ts
@@ -0,0 +1,40 @@
+import CanvasSprite from '../Canvas/CanvasSprite';
+import Vector from '../Math/Vector';
+
+export default class ImageLoader {
+    static readonly instance = new ImageLoader();
+    private images: Map<string, HTMLImageElement> = new Map();
+
+    private constructor() {}
+
+    static loadImages(images: string[]): Promise<void> {
+        return new Promise(resolve => {
+            let loaded = 0;
+            for (const image of images) {
+                const img = new Image();
+                img.src = `images/${image}`;
+                img.onload = () => {
+                    loaded++;
+                    if (loaded === images.length) {
+                        resolve();
+                    }
+                }
+                ImageLoader.instance.images.set(image, img);
+            }
+        });
+    }
+
+    fromName(image: string, offset: Vector, size: Vector, position: Vector): CanvasSprite {
+        return new CanvasSprite(ImageLoader.instance.getImage(image), offset, size, position);
+    }
+
+    getImage(image: string): HTMLImageElement {
+        const element = this.images.get(image);
+
+        if (!element) {
+            throw new Error(`Could not find image with name "${image}".`);
+        }
+
+        return element;
+    }
+}
