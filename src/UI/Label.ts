6cc22694f5675eca47fab5015ddb17979551ed3e
Daniel Mecke <daniel.mecke@cunningsoft.de>
2022-12-23 20:36:13 +0100

    allow labels to be aligned

--- src/UI/Label.ts
+++ src/UI/Label.ts
@@ -1,4 +1,5 @@
 import Font from '../Font/Font';
+import TextAlign from '../Font/TextAlign';
 import Vector from '../Math/Vector';
 
 export default class Label {
@@ -6,10 +7,11 @@ export default class Label {
         private readonly font: Font,
         private readonly text: string,
         private readonly position: Vector,
+        private readonly align: TextAlign = TextAlign.left,
     ) {
     }
 
     draw(ctx: CanvasRenderingContext2D): void {
-        this.font.at(this.position).alignCenter().text(this.text).draw(ctx);
+        this.font.at(this.position).align(this.align).text(this.text).draw(ctx);
     }
 }
