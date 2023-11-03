6640781699bf4886e6499314ca369f353c5b533c
Daniel Mecke <mecke@kigaroo.de>
2023-11-03 16:08:33 +0100

    added circle line intersection

--- src/Math/Circle.ts
+++ src/Math/Circle.ts
@@ -1,3 +1,4 @@
+import Line from './Line';
 import Rng from './Rng';
 import Vector from './Vector';
 
@@ -21,5 +22,38 @@ export default class Circle {
         const theta = Rng.instance.randomFloatBetween(0, 1) * 2 * Math.PI;
 
         return this.position.addX(r * Math.cos(theta)).addY(r * Math.sin(theta));
+    }
+
+    getIntersectionsWithLine(line: Line): Vector[] {
+        const dx = line.point2.x - line.point1.x;
+        const dy = line.point2.y - line.point1.y;
+
+        const A = dx * dx + dy * dy;
+        const B = 2 * (dx * (line.point1.x - this.position.x) + dy * (line.point1.y - this.position.y));
+        const C = (line.point1.x - this.position.x) * (line.point1.x - this.position.x) + (line.point1.y - this.position.y) * (line.point1.y - this.position.y) - this.radius * this.radius;
+
+        const discriminant = B * B - 4 * A * C;
+
+        if (discriminant < 0) { // no intersection
+            return [];
+        }
+
+        if (discriminant === 0) { // one intersection point
+            const t = -B / (2 * A);
+            const x = line.point1.x + t * dx;
+            const y = line.point1.y + t * dy;
+
+            return [new Vector(x, y)];
+        }
+
+        // two intersection points
+        const t1 = (-B + Math.sqrt(discriminant)) / (2 * A);
+        const t2 = (-B - Math.sqrt(discriminant)) / (2 * A);
+        const x1 = line.point1.x + t1 * dx;
+        const y1 = line.point1.y + t1 * dy;
+        const x2 = line.point1.x + t2 * dx;
+        const y2 = line.point1.y + t2 * dy;
+
+        return [new Vector(x1, y1), new Vector(x2, y2)];
     }
 }
--- test/Math/Circle.test.ts
+++ test/Math/Circle.test.ts
@@ -1,4 +1,5 @@
 import Circle from '../../src/Math/Circle';
+import Line from '../../src/Math/Line';
 import Vector from '../../src/Math/Vector';
 
 describe('Circle', () => {
@@ -25,5 +26,31 @@ describe('Circle', () => {
 
             expect(circle.contains(randomPoint)).toBe(true);
         }
+    });
+
+    describe('getIntersectionsWithLine', () => {
+        it('returns no intersections if the line does not intersect the circle', () => {
+            const circle = new Circle(new Vector(0, 0), 5);
+            const line = new Line(new Vector(-10, 10), new Vector(10, 10));
+
+            expect(circle.getIntersectionsWithLine(line)).toEqual([]);
+        });
+
+        it('returns one intersection if the line intersects the circle once', () => {
+            const circle = new Circle(new Vector(0, 0), 5);
+            const line = new Line(new Vector(-10, 5), new Vector(10, 5));
+            const intersection = new Vector(0, 5);
+
+            expect(circle.getIntersectionsWithLine(line)).toEqual([intersection]);
+        });
+
+        it('returns two intersections if the line intersects the circle twice', () => {
+            const circle = new Circle(new Vector(0, 0), 5);
+            const line = new Line(new Vector(-10, 0), new Vector(10, 0));
+            const intersectionA = new Vector(5, 0);
+            const intersectionB = new Vector(-5, 0);
+
+            expect(circle.getIntersectionsWithLine(line)).toEqual([intersectionA, intersectionB]);
+        });
     });
 });
